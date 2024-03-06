adfb1851f2b27ee50ab73834999125ca1b690f01
Alex Heywood <alexheywood@valeofglamorgan.gov.uk>
2024-03-06 17:04:53 +0000

    changed styling for process.jsx removed footer for now

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -3,7 +3,7 @@ import logo from '/logo.jpg'
 export default function Footer() {
 
     return (
-        <nav className="navbar fixed-bottom navbar-light bg-light text-center justify-content-center">
+        <nav className="navbar navbar-light bg-light text-center justify-content-center">
             <small><a className="navbar-brand" href="#">Vale of Glamorgan Council</a></small>
         </nav>
     )
--- client/src/Process.jsx
+++ client/src/Process.jsx
@@ -12,6 +12,7 @@ export default function Process() {
     const [description, setDescription] = useState('');
     const [serviceArea, setServiceArea] = useState('');
     const [owner, setOwner] = useState('');
+    const [hasDiscovery, setHasDiscovery] = useState(false)
     const [hasBuild, setHasBuild] = useState();
     const [hasTests, setHasTests] = useState();
     const [hasContent, setHasContent] = useState();
@@ -29,6 +30,7 @@ export default function Process() {
                 setDescription(res.data.description)
                 setServiceArea(res.data.serviceArea)
                 setOwner(res.data.owner)
+                setHasDiscovery(res.data.hasDiscovery)
                 setHasBuild(res.data.hasBuild)
                 setHasTests(res.data.hasTests)
                 setHasContent(res.data.hasContent)
@@ -44,67 +46,78 @@ export default function Process() {
 
 
     return (
-        <div className="container w-100">
-            <div className="container-md h-auto my-5">
-                <Link className="mb-2" to="/" alt="Go back"><BsArrowLeftSquareFill size={30} /></Link>
-                <Link to={`/update/${id}`} className="btn btn-success mx-4 btn-sm">Update</Link>
+
+        <div className="container mt-5">
+            <Link className="mb-2" to="/" alt="Go back"><BsArrowLeftSquareFill size={30} /></Link>
+            <Link to={`/update/${id}`} className="btn btn-success mx-4 btn-sm">Update</Link>
+            <div className="row">
                 <div className="my-3">
                     <h1>Process Details</h1>
-                    <small>Process ID: {id}</small><br />
-                    <small>URL: <a href={formattedURL}>{formattedURL}</a></small>
                 </div>
-                <table className="table">
-                    <tbody>
-                        <tr>
-                            <th scope="row">Name:</th>
-                            <td>{name}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Description:</th>
-                            <td>{description}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Service Area:</th>
-                            <td>{serviceArea}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Owner:</th>
-                            <td>{owner}</td>
-                        </tr>
-                    </tbody>
-                </table>
+            </div>
+            <div className="row">
+                <div className=" col-12 col-md-6">
+                    <div className="mb-3">
+                        <small>Process ID: {id}</small><br />
+                        <small>URL: <a href={formattedURL}>{formattedURL}</a></small>
+                    </div>
+                    <table className="table">
+                        <tbody>
+                            <tr>
+                                <th scope="row">Name:</th>
+                                <td>{name}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Description:</th>
+                                <td>{description}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Service Area:</th>
+                                <td>{serviceArea}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Owner:</th>
+                                <td>{owner}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className=" col-12 col-md-6">
+                    <h3>Checklist:</h3>
+                    <table className="table">
+                        <tbody>
+                            <tr>
+                                <th scope="row">Discovery completed?</th>
+                                <td>{hasDiscovery ? <BsCheckCircleFill color='green' /> : <BsFillXCircleFill color='red' />}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Build completed?</th>
+                                <td>{hasBuild ? <BsCheckCircleFill color='green' /> : <BsFillXCircleFill color='red' />}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Tests completed?</th>
+                                <td>{hasTests ? <BsCheckCircleFill color='green' /> : <BsFillXCircleFill color='red' />}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Content confirmed?</th>
+                                <td>{hasContent ? <BsCheckCircleFill color='green' /> : <BsFillXCircleFill color='red' />}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Translations complete?</th>
+                                <td>{hasTranslation ? <BsCheckCircleFill color='green' /> : <BsFillXCircleFill color='red' />}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Passed the verify script?</th>
+                                <td>{passedVerify ? <BsCheckCircleFill color='green' /> : <BsFillXCircleFill color='red' />}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Has the process been successfully migrated?</th>
+                                <td>{hasMigrated ? <BsCheckCircleFill color='green' /> : <BsFillXCircleFill color='red' />}</td>
+                            </tr>
 
-                <h3>Checklist:</h3>
-                <table className="table">
-                    <tbody>
-                        <tr>
-                            <th scope="row">Build completed?</th>
-                            <td>{hasBuild ? <BsCheckCircleFill color='green' /> : <BsFillXCircleFill color='red' />}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Tests completed?</th>
-                            <td>{hasTests ? <BsCheckCircleFill color='green' /> : <BsFillXCircleFill color='red' />}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Content confirmed?</th>
-                            <td>{hasContent ? <BsCheckCircleFill color='green' /> : <BsFillXCircleFill color='red' />}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Translations complete?</th>
-                            <td>{hasTranslation ? <BsCheckCircleFill color='green' /> : <BsFillXCircleFill color='red' />}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Passed the verify script?</th>
-                            <td>{passedVerify ? <BsCheckCircleFill color='green' /> : <BsFillXCircleFill color='red' />}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Has the process been successfully migrated?</th>
-                            <td>{hasMigrated ? <BsCheckCircleFill color='green' /> : <BsFillXCircleFill color='red' />}</td>
-                        </tr>
-
-                    </tbody>
-                </table>
-
+                        </tbody>
+                    </table>
+                </div>
 
             </div>
         </div>
